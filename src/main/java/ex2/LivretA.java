4a27845cd5157dafd86bc83c3f478aa330958f43
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-01-24 10:16:10 +0100

    ex02

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -4,6 +4,16 @@ public class LivretA extends CompteBancaire {
 
 	public LivretA(String type, double solde, double tauxRemuneration) {
 		super(type, solde, 0, tauxRemuneration);
+		this.decouvert = 0;
+	}
+	
+	public void appliquerRemuAnnuelle() {
+			this.solde = solde + solde * tauxRemuneration / 100;
+	}
+	
+	@Override
+	public String getType() {
+		return "LA";
 	}
 
 }
