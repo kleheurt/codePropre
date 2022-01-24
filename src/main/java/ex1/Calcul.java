4a3c3fdf5b729bc8c835fb8b4bea102234891116
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-01-24 11:32:59 +0100

    ex01

--- src/main/java/ex1/Calcul.java
+++ src/main/java/ex1/Calcul.java
@@ -3,7 +3,10 @@ package ex1;
 /** Classe de calcul */
 public class Calcul {
 
-	/** Retourne l'addition de deux entiers */
+	/** Retourne l'addition de deux entiers 
+	 * @param a premier entier 
+	 * @param b deuxième entier
+	 * @return la somme de a et b*/
 	public int additionner(int a, int b){
 		return a+b;
 	}
